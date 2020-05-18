322ad6591162251bafb2a054dd4d52d3220b3c45
WhoCanThisBe <54661565+WenHu87@users.noreply.github.com>
2020-05-18 14:11:07 +0200

    changed userId function

--- docs/userRegister.js
+++ docs/userRegister.js
@@ -1,40 +1,27 @@
-let userList = JSON.parse(window.localStorage.getItem("delegateTaskList"));
+let userList = JSON.parse(window.localStorage.getItem("UserList")) || [];
 
-
-
-
-//User IdGenerator
-function userID() {
-    let IdCounter = "";
-
-    let userList = JSON.parse(window.localStorage.getItem("UserList"));
-
-    if (userList === null || userList.length == 0) {
-        IdCounter = 4000
-    } else {
-        IdCounter = 4000 + userList.length
-    }
-
-    return IdCounter;
-
-}
-
-
-
-
-
+let userID = (function () {
+    var id = 4000; // This is the private persistent value
+    // The outer function returns a nested function that has access
+    // to the persistent value.  It is this nested function we're storing
+    // in the variable uniqueID above.
+    return function () { return id++; };  // Return and increment
+})(); // Invoke the outer function after defining it.
 
 
 function userRegister(event) {
     event.preventDefault();
 
-    const userNa = document.getElementById("userName").value;
-    const firstNa = document.getElementById("firstName").value;
-    const lastNa = document.getElementById("lastName").value;
+    const userName = document.getElementById("userName").value;
+    const firstName = document.getElementById("firstName").value;
+    const lastName = document.getElementById("lastName").value;
 
-    const userRegister = { id: userID(), userName: userNa, firstName: firstNa, lastName: lastNa };
 
-    const userList = JSON.parse(window.localStorage.getItem("UserList")) || [];
+
+
+    const userRegister = { userId: userID(), userName, firstName, lastName };
+
+    userList = JSON.parse(window.localStorage.getItem("UserList")) || [];
 
 
     userList.push(userRegister);
@@ -51,19 +38,30 @@ function userRegister(event) {
 
 
 
-class User {
 
-    static IdCounter = 1000;
+// function User(user, first, last) {
+//     this.userName = user;
+//     this.firstName = first;
+//     this.lastName = last;
 
-    constructor(userName, firstName, lastName) {
-        this.ID = user.IdCounter;
-        this.userName = userName;
-        this.firstName = firstName;
-        this.lastName = lastName;
+//     return userId() + userName + firstName + LastName
+// }
 
-    }
+// function UserSetup() {
+//     new User(userID(), "Are546", "Arne", "Nilsen");
+//     new User(userID(), "Nilse456", "Nils", "Johansen");
 
-}
+
+
+
+
+
+
+// }
+
+
+
+
 
 
 //https://stackoverflow.com/questions/24403732/check-if-array-is-empty-or-does-not-exist-js
