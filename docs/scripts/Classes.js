f97f5b6b57d2acc0b360de777128c0213e34dbcc
WhoCanThisBe <54661565+WenHu87@users.noreply.github.com>
2020-05-19 11:28:51 +0200

    new version

---
+++ docs/scripts/Classes.js
@@ -0,0 +1,40 @@
+
+class Task {
+
+
+    static taskArray = JSON.parse(window.localStorage.getItem("TaskList")) || [];
+
+
+    constructor(taskText, priority, startDate, endDate) {
+        this.taskId = this.idNumber();
+        this.taskText = taskText;
+        this.priority = priority;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        Task.taskArray.push(this);
+
+
+    }
+
+    idNumber() {
+        let IdCounter = "";
+
+        if (Task.taskArray.length == null) {
+            IdCounter = 4000;
+        } else {
+
+            IdCounter = 4000 + Task.taskArray.length
+        }
+
+        return IdCounter;
+
+    }
+
+
+    static setup() {
+
+        new Task("løø", "Low", "2020-05-18", "2020-05-21")
+
+    }
+
+}
