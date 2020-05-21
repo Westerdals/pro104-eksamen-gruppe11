731fe1f2ee7316de758f89ea82e4d2ae0c093d0f
Juni Halseth Stenberg <juni@lyse.net>
2020-05-21 16:23:16 +0200

    Fixed some bugs.
    * Bug 1: Projects were added to the list even though the error messages appeared
    * Bug 2: Projects were added to the list even though the project already existed in the list and error message appeared
    * Bug 3: Page refreshed after submit

--- docs/scripts/projectRegister.js
+++ docs/scripts/projectRegister.js
@@ -1,6 +1,3 @@
-
-
-
 //User IdGenerator
 function ProjectID() {
     let IdCounter = "";
@@ -26,13 +23,19 @@ function createProject(event) {
     const endDate = document.getElementById("endDate").value;
     const projectDesc = document.getElementById("projectDesc").value;
 
-
     const projectInfo = { ProjectID: ProjectID(), projectName, startDate, endDate, projectDesc, tasks: [], delegate: [], memberList: [] };
 
-    const projectList = JSON.parse(window.localStorage.getItem("Projects")) || [];
-    projectList.push(projectInfo);
-    window.localStorage.setItem("Projects", JSON.stringify(projectList));
+    const projectList = getAllProjectsFromLocalStorage();
 
+    // Don't add the project if form is not valid or the project name is duplicate
+    if(isValidProjectInput() && isNotDuplicateProjectName(projectName, projectList)){
+        projectList.push(projectInfo);
+        window.localStorage.setItem("Projects", JSON.stringify(projectList));
+
+        // Display the new member list and project list to the user.
+        createProjectDropdownList();
+        createMembersDropdownList();
+    }
 }
 
 
@@ -48,8 +51,7 @@ function addTaskProject(event) {
     const taskEndtDate = document.getElementById("taskEndtDate").value;
     const task = { taskText, priorities, taskStartDate, taskEndtDate }
 
-    // [] in project here is for situation where local storage is emtpy
-    const projects = JSON.parse(window.localStorage.getItem("Projects")) || [];
+    const projects = getAllProjectsFromLocalStorage();
 
     const lastProject = projects[projects.length - 1];
 
@@ -61,11 +63,14 @@ function addTaskProject(event) {
     event.target.reset();
 }
 
-// Adding members to prject
-const members = document.querySelector('#add-members-form__assigned-members');
-const projects = document.querySelector('#add-members-form__project');
-const projectList = JSON.parse(window.localStorage.getItem('Projects')) ?? [];
-const userList = JSON.parse(window.localStorage.getItem('UserList')) ?? [];
+function getAllMembersFromLocalStorage() {
+    return JSON.parse(window.localStorage.getItem('UserList')) ?? [];
+}
+
+function getAllProjectsFromLocalStorage() {
+    // [] in project here is for situation where local storage is emtpy
+    return JSON.parse(window.localStorage.getItem("Projects")) ?? [];
+}
 
 // function to create option for the select element
 const createOption = (parentElement, id, value) => {
@@ -75,37 +80,50 @@ const createOption = (parentElement, id, value) => {
     parentElement.appendChild(option);
 }
 
-for (const list of projectList) {
-    const { ProjectID, projectName } = list;
-    createOption(projects, ProjectID, projectName);
-}
-for (const list of userList) {
-    const { id, firstName, lastName } = list;
-    createOption(members, id, firstName + ' ' + lastName);
-}
+// create the dropdown menu for selecting member
+function createMembersDropdownList() {
+    const members = document.querySelector('#add-members-form__assigned-members');
+    const userList = getAllMembersFromLocalStorage();
 
-// Reloader the webpage after assigning a project name
-document.querySelector('#projectRegBtn').addEventListener('click', (e) => {
-    for (const list of projectList) {
-        const projectName = list.projectName;
-        const projectNameInput = document.querySelector('#projectName').value;
-
-        // Checking if projectName of the object is equal with the user inputing the text field
-        if (projectName == projectNameInput) {
-            throw "Project name already Exist";
-
-        } else {
-            location.reload();
-        }
+    // Make the dropdown empty before creating the new elements.
+    if(members.length != 0) {
+        while (members.lastElementChild) {
+            members.removeChild(members.lastElementChild);
+          }
     }
-})
+    for (const list of userList) {
+        const { id, firstName, lastName } = list;
+        createOption(members, id, firstName + ' ' + lastName);
+    }
+}
+
+// create the dropdown menu for selecting project
+function createProjectDropdownList() {
+    const projects = document.querySelector('#add-members-form__project');
+    const projectList = getAllProjectsFromLocalStorage();
+    
+    // Make the dropdown empty before creating the new elements.
+    if(projects.length != 0) {
+        while (projects.lastElementChild) {
+            projects.removeChild(projects.lastElementChild);
+          }
+    }
+
+    for (const list of projectList) {
+        const { ProjectID, projectName } = list;
+        createOption(projects, ProjectID, projectName);
+    }
+}
 
 // Event Listener for delegating members to a project
 document.querySelector('.add-members-form__submit').addEventListener('click', (e) => {
     e.preventDefault();
+    const members = document.querySelector('#add-members-form__assigned-members');
+    const projects = document.querySelector('#add-members-form__project');
     const userValue = members.value;
     const projectValue = projects.value;
     const memberList = { userId: userValue, projectId: projectValue };
+    const projectList = getAllProjectsFromLocalStorage();
     const checkUserExist = projectList[0].memberList.some(user => user.userId === userValue)
 
     if (checkUserExist) {
@@ -117,17 +135,14 @@ document.querySelector('.add-members-form__submit').addEventListener('click', (e
     }
 });
 
-
-
-
 /**
  * For some() funksjon
  * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
  */
 
 // Add task Popup by changing display
-//if no input in the form the next form wont show
-document.querySelector(".add-task-project__submit").addEventListener('click', () => {
+//if no input in the form the next form wont show. Returns true if project is valid
+function isValidProjectInput() {
 
     if (document.getElementById("projectName").value != "" && document.getElementById("startDate").value != "" &&
         document.getElementById("endDate").value != "" && document.getElementById("projectDesc").value != "") {
@@ -138,28 +153,32 @@ document.querySelector(".add-task-project__submit").addEventListener('click', ()
         document.querySelector(".add-delegate-project__submit").addEventListener('click', () => {
             if (document.getElementById("taskText").value != "" && document.getElementById("priorities").value != ""
                 && document.getElementById("taskStartDate").value != "" && document.getElementById("taskEndtDate").value != "") {
-
                 document.querySelector(".delegate-form").style.display = "block";
-            } else alert("please fill the blank");
+            } else {
+                alert("please fill the blank");
+                return false;
+            }
         })
-
+        return true;
     } else {
-
         alert("please fill the blank");
-
+        return false;
     }
-})
+}
 
+// returns true if the project name is not in the project list. We do not want duplicate project names.
+function isNotDuplicateProjectName(projectName, projectList) {
+    const duplicateProjectName = projectList.filter(project => project.projectName == projectName) ?? [];
+    if(duplicateProjectName.length != 0) {
+        console.error(`Project with name: ${projectName} already exists.`);
+        // TODO: Disply the text to the user?
+        return false;
+    } else return true;
+}
 
-
-
-
-
-
-
-
-
-
+// Fill the members & project dropdown once the page loads
+createMembersDropdownList();
+createProjectDropdownList();
 
 //https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelector
 //http://getbem.com/naming/
@@ -170,4 +189,3 @@ document.querySelector(".add-task-project__submit").addEventListener('click', ()
 
 //search in array
 //https://www.w3schools.com/jsref/jsref_find.asp
-
