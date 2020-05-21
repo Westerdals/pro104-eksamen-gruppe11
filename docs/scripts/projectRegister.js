2cad9e4c5cbee1c16a9ac623c354a9ef3a385f71
WhoCanThisBe <54661565+WenHu87@users.noreply.github.com>
2020-05-21 13:55:34 +0200

    added add teammember in projecregister and function, taask register, add popup after filling forms and removes task after switching project in taskregister

--- docs/scripts/projectRegister.js
+++ docs/scripts/projectRegister.js
@@ -27,7 +27,7 @@ function createProject(event) {
     const projectDesc = document.getElementById("projectDesc").value;
 
 
-    const projectInfo = { ProjectID: ProjectID(), projectName, startDate, endDate, projectDesc, tasks: [] };
+    const projectInfo = { ProjectID: ProjectID(), projectName, startDate, endDate, projectDesc, task: [], delegate: [], memberList: [] };
 
     const projectList = JSON.parse(window.localStorage.getItem("Projects")) || [];
     projectList.push(projectInfo);
@@ -61,18 +61,79 @@ function addTaskProject(event) {
     event.target.reset();
 }
 
+// Adding members to prject
+const members = document.querySelector('#add-members-form__assigned-members');
+const projects = document.querySelector('#add-members-form__project');
+const projectList = JSON.parse(window.localStorage.getItem('Projects')) ?? [];
+const userList = JSON.parse(window.localStorage.getItem('UserList')) ?? [];
 
+// function to create option for the select element
+const createOption = (parentElement, id, value) => {
+    const option = document.createElement('option');
+    option.setAttribute('value', id);
+    option.textContent = value;
+    parentElement.appendChild(option);
+}
+
+for (const list of projectList) {
+    const { ProjectID, projectName } = list;
+    createOption(projects, ProjectID, projectName);
+}
+for (const list of userList) {
+    const { id, firstName, lastName } = list;
+    createOption(members, id, firstName + ' ' + lastName);
+}
+
+// Reloader the webpage after assigning a project name
+document.querySelector('#projectRegBtn').addEventListener('click', (e) => {
+    for (const list of projectList) {
+        const projectName = list.projectName;
+        const projectNameInput = document.querySelector('#projectName').value;
+
+        // Checking if projectName of the object is equal with the user inputing the text field
+        if (projectName == projectNameInput) {
+            throw "Project name already Exist";
+
+        } else {
+            location.reload();
+        }
+    }
+})
+
+// Event Listener for delegating members to a project
+document.querySelector('.add-members-form__submit').addEventListener('click', (e) => {
+    e.preventDefault();
+    const userValue = members.value;
+    const projectValue = projects.value;
+    const memberList = { userId: userValue, projectId: projectValue };
+    const checkUserExist = projectList[0].memberList.some(user => user.userId === userValue)
+
+    if (checkUserExist) {
+        throw 'User Already Exist!';
+    } else {
+        console.log('Added User to the memberList');
+        projectList[0].memberList.push(memberList);
+        window.localStorage.setItem('Projects', JSON.stringify(projectList));
+    }
+});
+
+
+
+
+/**
+ * For some() funksjon
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/some
+ */
 
 // Add task Popup by changing display
 //if no input in the form the next form wont show
 document.querySelector(".add-task-project__submit").addEventListener('click', () => {
 
-
-
     if (document.getElementById("projectName").value != "" && document.getElementById("startDate").value != "" &&
         document.getElementById("endDate").value != "" && document.getElementById("projectDesc").value != "") {
 
         document.querySelector(".add-task-form").style.display = "block";
+
         // Add delegate task Popup by changing display
         document.querySelector(".add-delegate-project__submit").addEventListener('click', () => {
             if (document.getElementById("taskText").value != "" && document.getElementById("priorities").value != ""
@@ -85,9 +146,6 @@ document.querySelector(".add-task-project__submit").addEventListener('click', ()
     } else {
 
         alert("please fill the blank");
-
-
-
 
     }
 })
--- docs/scripts/taskRegister.js
+++ docs/scripts/taskRegister.js
@@ -1,92 +1,127 @@
-
-
 //Show the created project in the datalist
-function projectListShow() {
-    const projectList = JSON.parse(window.localStorage.getItem("Projects")) ?? [];
 
-    const projectListEl = document.getElementById("createdProject");
+const projectList = JSON.parse(window.localStorage.getItem('Projects')) ?? [];
 
-    for (const projects of projectList) {
-        projectListEl.innerHTML += `<option value = ${projects.ProjectID}>${projects.projectName}</option>"`;
-    }
+let projectListEl = '';
+projectListEl = document.getElementById('createdProject');
+
+for (const projects of projectList) {
+    projectListEl.innerHTML += `<option value = ${projects.ProjectID}>${projects.projectName}</option>"`;
 }
 
-//Creates new task
+//User IdGenerator
+function taskGen() {
+    taskId();
+
+    let counter = JSON.parse(window.localStorage.getItem('taskCounter'));
+
+    IdCounter = 1999 + counter.length;
+
+    return IdCounter;
+}
+
+//fake localstorage to a store id numbers
+function taskId() {
+    const counter = JSON.parse(window.localStorage.getItem('taskCounter')) || [];
+    counter.push('0');
+    window.localStorage.setItem('taskCounter', JSON.stringify(counter));
+}
+
+// Creates new task
 function createTask(event) {
     event.preventDefault();
+    const taskText = document.getElementById('taskText').value;
+    const priorities = document.getElementById('priorities').value;
+    const taskStartDate = document.getElementById('taskStartDate').value;
+    const taskEndtDate = document.getElementById('taskEndtDate').value;
 
-    const taskText = document.getElementById("taskText").value;
-    const priorities = document.getElementById("priorities").value;
-    const taskStartDate = document.getElementById("taskStartDate").value;
-    const taskEndtDate = document.getElementById("taskEndtDate").value;
-
-    const task = { ID: new ID(), taskText, priorities, taskStartDate, taskEndtDate }
+    const task = {
+        id: taskGen(),
+        taskText,
+        priorities,
+        taskStartDate,
+        taskEndtDate,
+        delegate: [],
+        memberList: []
+    };
 
     attachTaskToProj(task);
 
     event.target.reset();
 }
 
-
 //Attach new task to project
 function attachTaskToProj(task) {
+    const createdProject = document.getElementById('createdProject').value;
 
-    const createdProject = document.getElementById("createdProject").value;
-
-    const projectList = JSON.parse(window.localStorage.getItem("Projects"));
+    const projectList = JSON.parse(window.localStorage.getItem('Projects'));
 
     //for each loop
     projectList.forEach((el) => {
-        console.log(el.ProjectID);
         if (el.ProjectID == createdProject) {
-            el.task.push(task)
+            el.tasks.push(task);
         }
-    })
+    });
 
-    console.log(projectList);
-
-    window.localStorage.setItem("Projects", JSON.stringify(projectList));
+    window.localStorage.setItem('Projects', JSON.stringify(projectList));
 }
 
 
-let idCounter = 2000;
-
-//User IdGenerator
-function taskID() {
-
-    return idCounter++;
-}
-
-//Pushes created task to list
-// function showTaskList() {
-//     const projectTaskList = JSON.parse(window.localStorage.getItem("Projects")) || [];
-
-//     const taskListEl = document.getElementById("taskList");
-//     taskListEl.innerHTML = "";
-
-//     for (const project of projectTaskList) {
-//         for (const task of project) {
-
-//             taskListEl.innerHTML += `<option value = ${task.taskText}>${task.taskText}</option>"`;
-//         }
-//     }
-// }
-
-
-//showTaskList()
-projectListShow()
-
-
-class ID {
-
-    static idCounter = 0;
-
-    constructor() {
-        this.ID = ID.idCounter++;
+//removes taskList after chosen
+function removeOptions(selectElement) {
+    var i,
+        L = selectElement.options.length - 1;
+    for (i = L; i >= 0; i--) {
+        selectElement.remove(i);
     }
-
-
-
 }
 
+const createOption = (parentEl, id, value) => {
+    // Create Option Element
+    let option = document.createElement('option');
+    option.setAttribute('value', id);
+    option.innerHTML = value;
+    parentEl.appendChild(option);
+};
 
+//task popup version
+
+//shows creating task
+document
+    .querySelector('.create-task-form-submit__submit')
+    .addEventListener('click', () => {
+        document.querySelector('.delegate-form-submit').style.display = 'block';
+    });
+
+document.querySelector('.create-task-form-submit').style.display = 'none';
+document.querySelector('.delegate-form-submit').style.display = 'none';
+const taskList = document.querySelector('#taskList');
+
+document.querySelector('#createdProject').addEventListener('change', (e) => {
+    removeOptions(taskList);
+
+    const projectList = JSON.parse(window.localStorage.getItem('Projects')) ?? [];
+
+    for (const projects of projectList) {
+        if (e.target.value == projects.ProjectID) {
+            document.querySelector(
+                '.create-task-form-submit__h4',
+            ).innerHTML = `Opprett Oppgave ${projects.ProjectID}`;
+            document.querySelector('.create-task-form-submit').style.display =
+                'block';
+
+            for (const task of projects.tasks) {
+                let { id, taskText } = task;
+                createOption(taskList, id, taskText);
+            }
+
+            document
+                .querySelector('.delegate-form-submit__legg--til')
+                .addEventListener('click', (e) => {
+                    e.preventDefault();
+                    const userList = document.querySelector('#userList').value;
+                    const taskList = document.querySelector('#taskList').value;
+                });
+        }
+    }
+});
