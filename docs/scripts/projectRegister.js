d57dc24a20c1bc6c5e121b63da2f1a6eddfdca7a
Junistenberg <juni@lyse.net>
2020-05-26 21:01:14 +0200

    Remove add task to project from create project page
    We decided only to have this functionality on the project manager board.

--- docs/scripts/projectRegister.js
+++ docs/scripts/projectRegister.js
@@ -1,17 +1,14 @@
 //User IdGenerator
 function ProjectID() {
-    let IdCounter = "";
-
-    let projects = JSON.parse(window.localStorage.getItem("Projects"));
+    let IdCounter = 0;
+    const projects = getProjects();
 
     if (projects === null || projects.length == 0) {
         IdCounter = 2000
     } else {
         IdCounter = 2000 + projects.length
     }
-
     return IdCounter;
-
 }
 
 //Creating new project
@@ -34,64 +31,27 @@ function createProject(event) {
         saveProjects(projectList);
         showStatusMessage("Project created.", true);
 
-        // Display the new member list to the user.
-        showAddProjectDetails();
+        // Re-render the project list and close
         renderProjectList();
+        closeCreateProjectForm(event);
     }
 
 }
 
-// This function is a part of projectregister popup. Add task to the new created project.
-
-function addTaskProject(event) {
-
-    event.preventDefault();
-
-    const taskText = document.getElementById("taskText").value;
-    const priorities = document.getElementById("priorities").value;
-    const taskStartDate = document.getElementById("taskStartDate").value;
-    const taskEndDate = document.getElementById("taskEndDate").value;
-    const task = { id: generateUuid(), taskText, priorities, taskStartDate, taskEndtDate: taskEndDate, delegate: [] }
-
-    const projects = getProjects();
-
-    const lastProject = projects[projects.length - 1];
-
-    if (isTaskDateValidForProject(taskStartDate, taskEndDate, lastProject)) {
-        lastProject.tasks.push(task);
-        saveProjects(projects)
-        event.target.reset();
-        showStatusMessage("Added task to project", true);
-    }
+function closeCreateProjectForm(event) {
+    event.target.reset();
+    const createProjectFormDiv = document.getElementById('add-project-form');
+    createProjectFormDiv.style.display = 'none';
 }
 
-// function to create option for the select element
-const createOption = (parentElement, id, value) => {
-    const option = document.createElement('option');
-    option.setAttribute('value', id);
-    option.textContent = value;
-    parentElement.appendChild(option);
-}
-
-// Add task Popup by changing display
-//if no input in the form the next form wont show. Returns true if project is valid
+// Returns true if all fields has content
 function isValidProjectInput() {
+    const projectName = document.getElementById("projectName").value;
+    const startDate = document.getElementById("startDate").value;
+    const endDate = document.getElementById("endDate").value;
+    const projectDesc = document.getElementById("projectDesc").value
 
-    if (document.getElementById("projectName").value != "" && document.getElementById("startDate").value != "" &&
-        document.getElementById("endDate").value != "" && document.getElementById("projectDesc").value != "") {
-
-        document.querySelector(".add-task-form").style.display = "block";
-
-        // Add delegate task Popup by changing display
-        document.querySelector(".add-delegate-project__submit").addEventListener('click', () => {
-            if (document.getElementById("taskText").value != "" && document.getElementById("priorities").value != ""
-                && document.getElementById("taskStartDate").value != "" && document.getElementById("taskEndDate").value != "") {
-                return true;
-            } else {
-                showStatusMessage("Please fill out the blanks..", false);
-                return false;
-            }
-        })
+    if (projectName != "" && startDate != "" && endDate != "" && projectDesc != "") {
         return true;
     } else {
         showStatusMessage("Please fill out the blanks..", false);
@@ -99,7 +59,7 @@ function isValidProjectInput() {
     }
 }
 
-// returns true if the project name is not in the project list. We do not want duplicate project names.
+// Returns true if the project name is not in the project list. We do not want duplicate project names.
 function isNotDuplicateProjectName(projectName, projectList) {
     const duplicateProjectName = projectList.filter(project => project.projectName == projectName) ?? [];
 
@@ -122,33 +82,7 @@ function isProjectDateValid(projectStartDateAsString, projectEndDateAsString) {
 
 }
 
-// returns true if the task start and end date is within the projects dates
-function isTaskDateValidForProject(taskStartDateAsString, taskEndDateAsString, project) {
-    const taskStartDate = new Date(taskStartDateAsString);
-    const taskEndDate = new Date(taskEndDateAsString);
-    const projectStartDate = new Date(project.startDate);
-    const projectEndDate = new Date(project.endDate);
-
-
-    if (taskStartDate < projectStartDate) {
-        showStatusMessage("Task can't start before project start..", false);
-        return false;
-    } else if (taskEndDate > projectEndDate) {
-        showStatusMessage("Task can't end after project end..", false);
-        return false;
-    } else if (taskStartDate > taskEndDate) {
-        showStatusMessage("Task can't end before start date..", false);
-        return false;
-    } else {
-        return true;
-    }
-}
-
-function showAddProjectDetails() {
-    document.getElementById('add-project-details-container').style = "block";
-}
-
-// function that renders prject list to the page. 
+// Function that renders project list to the page. 
 function renderProjectList() {
 
     const projectList = getProjects();
@@ -172,14 +106,14 @@ function renderProjectList() {
 
         //The divs containing the project information is assinged the class projectBoxes
         projectEl.classList.add(`projectBoxes`);
-        projectEl.style.cursor = "pointer"; // TODO: probably better with css.
+        projectEl.style.cursor = "pointer";
     }
 }
 
 // render all the projects on page load
 renderProjectList();
 
-//Icon project adder
-function addProjectForm() {
+// Icon project adder
+function openProjectForm() {
     document.getElementById("add-project-form").style = "block";
 }
